b40d5016ce1a20fcd0b0638df85bd2e2e3733392
Ardha Putra <ardhakrs@gmail.com>
2026-01-03 00:07:11 +0800

    coding some  models

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,9 +2,55 @@
 
 namespace App\Models;
 
+use illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Attributes\Scope;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Brand extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'logo',
+        'website',
+        'is_active',
+        'sort_order',
+    ];
+
+    #[Scope()]
+    protected function active(Builder $builder)
+    {
+        $builder->where('is_active', true);
+    }
+
+    #[Scope()]
+    protected function sorted(Builder $builder)
+    {
+        $builder->orderBy('sort_order', 'asc');
+    }
+
+    // relationship
+    public function products()
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    protected static function boot() 
+    {
+        parent::boot();
+
+        static::creating(function($brand) {
+            if (empty($brand->slug)) {
+                $brand->slug = Str::slug($brand->name);
+            }
+        });
+
+        static::updating(function($brand) {
+            if ($brand->isDirty('name') && empty($brand->empty)) {
+                $brand->slug = Str::slug($brand->name);
+            }
+        }); 
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,34 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Str;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'phone',
+        'is_active',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'two_factor_secret',
@@ -37,22 +30,15 @@ class User extends Authenticatable
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 
-    /**
-     * Get the user's initials
-     */
     public function initials(): string
     {
         return Str::of($this->name)
@@ -60,5 +46,22 @@ class User extends Authenticatable
             ->take(2)
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
+    }
+
+    // Local scope
+    public function scopeActive(Builder $builder): Builder
+    {
+        return $builder->where('is_active', true);
+    }
+
+    // relationship
+    public function orderStatusHistories()
+    {
+        return $this->hasMany(OrderStatusHistory::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@example.com');
     }
 }
